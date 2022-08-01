360f7605b6ff2092776d197554f097577e098bc8
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-31 19:13:37 -0600

    feat: add methods total cost and breakout for iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,4 +10,30 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    total_activities_cost = 0
+
+    @activities.each do |activity|
+      total_activities_cost += activity.total_cost
+    end
+    total_activities_cost
+  end
+
+  def breakout
+    reunion_breakout = Hash.new(0)
+
+    @activities.each do |activity|
+      owed = activity.owed
+      activity.participants.each do |participant, cost|
+        reunion_breakout[participant] += owed[participant]
+      end
+    end
+    reunion_breakout
+  end
+
+  def summary
+
+    
+  end
 end
--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -30,45 +30,45 @@ RSpec.describe Activity do
 
   context 'Iteration 3' do
     before :each do
-      activity_1 = Activity.new("Brunch")
-      activity_2 = Activity.new("Drinks")
+      @activity_1 = Activity.new("Brunch")
+      @activity_2 = Activity.new("Drinks")
     end
 
     it 'reunion has a total cost' do
-      activity_1.add_participant("Maria", 20)
-      activity_1.add_participant("Luther", 40)
-      activity_2.add_participant("Maria", 60)
-      activity_2.add_participant("Luther", 60)
-      activity_2.add_participant("Louis", 0)
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @activity_2.add_participant("Maria", 60)
+      @activity_2.add_participant("Luther", 60)
+      @activity_2.add_participant("Louis", 0)
 
-      @reunion.add_activity(activity_1)
-      @reunion.add_activity(activity_2)
+      @reunion.add_activity(@activity_1)
+      @reunion.add_activity(@activity_2)
 
       expect(@reunion.total_cost).to eq(180)
     end
 
     it 'reunion can return how much participants are owed' do
-      activity_1.add_participant("Maria", 20)
-      activity_1.add_participant("Luther", 40)
-      activity_2.add_participant("Maria", 60)
-      activity_2.add_participant("Luther", 60)
-      activity_2.add_participant("Louis", 0)
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @activity_2.add_participant("Maria", 60)
+      @activity_2.add_participant("Luther", 60)
+      @activity_2.add_participant("Louis", 0)
 
-      @reunion.add_activity(activity_1)
-      @reunion.add_activity(activity_2)
+      @reunion.add_activity(@activity_1)
+      @reunion.add_activity(@activity_2)
 
       expect(@reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
     end
 
     it 'reunion can return a summary' do
-      activity_1.add_participant("Maria", 20)
-      activity_1.add_participant("Luther", 40)
-      activity_2.add_participant("Maria", 60)
-      activity_2.add_participant("Luther", 60)
-      activity_2.add_participant("Louis", 0)
+      @activity_1.add_participant("Maria", 20)
+      @activity_1.add_participant("Luther", 40)
+      @activity_2.add_participant("Maria", 60)
+      @activity_2.add_participant("Luther", 60)
+      @activity_2.add_participant("Louis", 0)
 
-      @reunion.add_activity(activity_1)
-      @reunion.add_activity(activity_2)
+      @reunion.add_activity(@activity_1)
+      @reunion.add_activity(@activity_2)
 
       expect(@reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
     end
