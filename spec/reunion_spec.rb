88f3db6dd2f6642065a0788294a7d743431a66f7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-31 18:45:43 -0600

    feat: add tests for iteration 3

--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -1,4 +1,5 @@
 require './lib/reunion'
+require './lib/activity'
 require 'rspec'
 
 RSpec.describe Activity do
@@ -24,6 +25,52 @@ RSpec.describe Activity do
 
       @reunion.add_activity(activity_1)
       expect(@reunion.activities).to eq([activity_1])
+    end
+  end
+
+  context 'Iteration 3' do
+    before :each do
+      activity_1 = Activity.new("Brunch")
+      activity_2 = Activity.new("Drinks")
+    end
+
+    it 'reunion has a total cost' do
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+      activity_2.add_participant("Maria", 60)
+      activity_2.add_participant("Luther", 60)
+      activity_2.add_participant("Louis", 0)
+
+      @reunion.add_activity(activity_1)
+      @reunion.add_activity(activity_2)
+
+      expect(@reunion.total_cost).to eq(180)
+    end
+
+    it 'reunion can return how much participants are owed' do
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+      activity_2.add_participant("Maria", 60)
+      activity_2.add_participant("Luther", 60)
+      activity_2.add_participant("Louis", 0)
+
+      @reunion.add_activity(activity_1)
+      @reunion.add_activity(activity_2)
+
+      expect(@reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
+    end
+
+    it 'reunion can return a summary' do
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+      activity_2.add_participant("Maria", 60)
+      activity_2.add_participant("Luther", 60)
+      activity_2.add_participant("Louis", 0)
+
+      @reunion.add_activity(activity_1)
+      @reunion.add_activity(activity_2)
+
+      expect(@reunion.summary).to eq("Maria: -10\nLuther: -30\nLouis: 40")
     end
   end
 end
